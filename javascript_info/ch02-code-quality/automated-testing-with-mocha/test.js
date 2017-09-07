5aadd8b8c2a8f2e763e553f14f60662de1c1e9dd
kitiya <kitiya@users.noreply.github.com>
2017-09-07 12:44:24 -0600

    Data Types: Numbers
    Exercises

--- javascript_info/ch02-code-quality/automated-testing-with-mocha/test.js
+++ javascript_info/ch02-code-quality/automated-testing-with-mocha/test.js
@@ -73,13 +73,13 @@ describe("pow", function() {
 
 describe("pow", function() {
 
-	it("2 raised to power 3 is 8", function() {
-		assert.equal(pow(2, 3), 8);
-	});
+  it("2 raised to power 3 is 8", function() {
+    assert.equal(pow(2, 3), 8);
+  });
 
-	it("3 raised to power 3 is 27", function() {
-		assert.equal(pow(3, 3), 27);
-	});
+  it("3 raised to power 3 is 27", function() {
+    assert.equal(pow(3, 3), 27);
+  });
 
 });
 */
